3ba901bd68ace4634a6411ba6adeab87a57bfe6d
Astro-physici1 <alex-bougault@orange.fr>
2023-06-13 18:45:26 +0200

    Petits changements
    suite de la vidéo.

--- Moran le Jeu/Assets/GM.cs
+++ Moran le Jeu/Assets/GM.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GM : MonoBehaviour
 {
     public static float vertVel = 0;
     public static int coinTotal = 0;
+    public static float timeTotal = 0;
+    public float waittoload = 0;
+    public static string lvlCompStatus = "";
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +20,15 @@ public class GM : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        timeTotal += Time.deltaTime;
+    
+        if (lvlCompStatus == "fail")
+        {
+        waittoload += Time.deltaTime;
+        }
+        if (waittoload > 2)
+        {
+        SceneManager.LoadScene ("Level 2");
+        }
     }
 }
