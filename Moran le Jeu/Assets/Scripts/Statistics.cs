3b13808c18dfdea34566708df333b73e77cc620a
Astro-physici1 <alex-bougault@orange.fr>
2023-06-17 11:59:01 +0200

    Gros changements
    C'est de nouveau jouable correctement.

--- Moran le Jeu/Assets/Scripts/Statistics.cs
+++ Moran le Jeu/Assets/Scripts/Statistics.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Stats : MonoBehaviour
+public class Statistics : MonoBehaviour
 
 {
     void Update()
