be5495a34111db4fadbffe3baf9857d87f4206d8
Astro-physici1 <alex-bougault@orange.fr>
2023-06-17 22:47:59 +0200

    Début de l'infini

--- Moran le Jeu/Assets/Scripts/RandomPrefabs.cs
+++ Moran le Jeu/Assets/Scripts/RandomPrefabs.cs
@@ -4,25 +4,56 @@ using UnityEngine;
 
 public class RandomPrefabs : MonoBehaviour
 {
+//----Variables------------------------------------------------------------------
+    public GameObject[] parcourPrefabsDepart;
     public GameObject[] parcourPrefabs;
-    public float distancePrefabs
+    private float distancePrefabs = 36.5f;
+    private float distancePrefabsSafe = 0.0f;
+    private float distanceDepartPrefabSafe = 36.5f; // Bien différencier avec le Depart !
+    public GameObject prefabSafe;
 
+//-------------------------------------------------------------------------------
     void Start()
     {
-        
+        spawnRandomPrefabsDepart();
     }
 
+//-------------------------------------------------------------------------------
     void Update()
     {
-        if (Input.GetKeyDown (KeyCode.S))
-        {
+       if (Input.GetKeyDown (KeyCode.E))
+       {
             spawnRandomPrefabs();
-        }
+       }
     }
 
-    void spawnRandomPrefabs()
+//-------------------------------------------------------------------------------
+    void spawnRandomPrefabs() // Utlisé pour le reste de la partie.
     {
-        int prefabsIndex = RandomPrefabs.Range (0, parcourPrefabs.length);
-        Vector3 spawnPosition = new Vector3 (0, 0, )
+        // On créé une fonction Random pour que les prefabs soit aléatoires.
+        int prefabsIndex = Random.Range(0, parcourPrefabs.Length);
+        // On écrit la distance correspondante au prefab créé.
+        distancePrefabs += 73.0f;
+        distancePrefabsSafe = distancePrefabs + 36.5f;
+        Vector3 spawnPosition = new Vector3 (0, 0, distancePrefabs);
+        Vector3 spawnPositionPrefabSafe = new Vector3 (0, 0, distancePrefabsSafe);
+
+        // On créé le prefab sur la scene.
+        Instantiate (parcourPrefabs[prefabsIndex], spawnPosition, parcourPrefabs[prefabsIndex].transform.rotation);
+        Instantiate (prefabSafe, spawnPositionPrefabSafe, prefabSafe.transform.rotation);
+    }
+
+    void spawnRandomPrefabsDepart() // Utilisé uniquement au début pour plus d'originalité par la suite.
+    {
+        // On créé une fonction Random pour que les prefabs soit aléatoires.
+        int prefabsDepartIndex = Random.Range(0, parcourPrefabsDepart.Length);
+        // On écrit la distance correspondante au prefab créé.
+        Vector3 spawnPositionDepart = new Vector3 (0, 0, distanceDepartPrefabSafe);
+        Vector3 spawnPositionDepartPrefabSafe = new Vector3 (0, 0, distanceDepartPrefabSafe += 36.5f);
+
+        // On créé le prefab sur la scene.
+        Instantiate (parcourPrefabsDepart[prefabsDepartIndex], spawnPositionDepart,
+        parcourPrefabsDepart[prefabsDepartIndex].transform.rotation);
+        Instantiate (prefabSafe, spawnPositionDepartPrefabSafe, prefabSafe.transform.rotation);
     }
 }
